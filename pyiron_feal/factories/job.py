06133ddeb2997ba86b272af7d7314601d0980ba7
Liam Huber <liam.huber@gmail.com>
2021-06-23 17:18:09 +0200

    Add pressure kwarg

--- pyiron_feal/factories/job.py
+++ pyiron_feal/factories/job.py
@@ -32,100 +32,111 @@ class _Minimize(HasProject):
         super().__init__(project)
         self.name = JobName('min')
 
-    def _lammps_minimization(self, potl_index, name, structure):
+    def _lammps_minimization(self, potl_index, name, structure, pressure):
         job = self.project.create.job.Lammps(name)
         job.structure = structure
         job.potential = self.project.input.potentials[potl_index]
-        job.calc_minimize()
+        job.calc_minimize(pressure=pressure)
         return job
 
-    def BCC(self, potl_index=0, a=None, repeat=1):
+    def BCC(self, potl_index=0, a=None, repeat=1, pressure=0):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).BCC.a(a).repeat(repeat).string,
-            structure=self.project.create.structure.FeAl.BCC(a=a).repeat(repeat)
+            structure=self.project.create.structure.FeAl.BCC(a=a).repeat(repeat),
+            pressure=pressure
         )
 
-    def FCC(self, potl_index=0, a=None, repeat=1):
+    def FCC(self, potl_index=0, a=None, repeat=1, pressure=0):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).FCC.a(a).repeat(repeat).string,
-            structure=self.project.create.structure.FeAl.FCC(a=a).repeat(repeat)
+            structure=self.project.create.structure.FeAl.FCC(a=a).repeat(repeat),
+            pressure=pressure
         )
 
-    def random_BCC(self, potl_index=0, a=None, repeat=2, Al_at_frac=None):
+    def random_BCC(self, potl_index=0, a=None, repeat=2, Al_at_frac=None, pressure=0):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).random_BCC.a(a).repeat(repeat).c_Al(Al_at_frac).string,
-            structure=self.project.create.structure.FeAl.random_BCC(a=a, repeat=repeat, Al_at_frac=Al_at_frac)
+            structure=self.project.create.structure.FeAl.random_BCC(a=a, repeat=repeat, Al_at_frac=Al_at_frac),
+            pressure=pressure
         )
 
-    def B2(self, potl_index=0, a=None, repeat=1):
+    def B2(self, potl_index=0, a=None, repeat=1, pressure=0):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).B2.a(a).repeat(repeat).string,
-            structure=self.project.create.structure.FeAl.B2(a=a).repeat(repeat)
+            structure=self.project.create.structure.FeAl.B2(a=a).repeat(repeat),
+            pressure=pressure
         )
 
-    def D03(self, potl_index=0, a=None, repeat=1):
+    def D03(self, potl_index=0, a=None, repeat=1, pressure=0):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.a(a).repeat(repeat).string,
-            structure=self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
+            structure=self.project.create.structure.FeAl.D03(a=a).repeat(repeat),
+            pressure=pressure
         )
 
-    def dilute(self, potl_index=0, a=None, repeat=2):
+    def dilute(self, potl_index=0, a=None, repeat=2, pressure=0):
         structure = self.project.create.structure.FeAl.BCC(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).BCC.append('Al').a(a).repeat(repeat).string,
-            structure=structure
+            structure=structure,
+            pressure=pressure
         )
 
-    def B2_anti_Fe_to_Al(self, potl_index=0, a=None, repeat=2):
+    def B2_anti_Fe_to_Al(self, potl_index=0, a=None, repeat=2, pressure=0):
         structure = self.project.create.structure.FeAl.B2(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
-            name=self.name.potl(potl_index).B2.append('Al').a(a).repeat(repeat).string,
-            structure=structure
+            name=self.name.potl(potl_index).B2.append('Al').a(a).repeat(repeat).P(pressure).string,
+            structure=structure,
+            pressure=pressure
         )
 
-    def B2_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=2):
+    def B2_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=2, pressure=0):
         structure = self.project.create.structure.FeAl.B2(a=a).repeat(repeat)
         structure[1] = 'Fe'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).B2.append('Fe').a(a).repeat(repeat).string,
-            structure=structure
+            structure=structure,
+            pressure=pressure
         )
 
-    def D03_anti_aFe_to_Al(self, potl_index=0, a=None, repeat=1):
+    def D03_anti_aFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('aAl').a(a).repeat(repeat).string,
-            structure=structure
+            structure=structure,
+            pressure=pressure
         )
 
     # The two unique Fe sites were found in a by-hand analysis of the chemistry of the first two neighbour shells and
     # then a brute-force calculation of the energy which simply confirmed the observed symmetries.
-    def D03_anti_bFe_to_Al(self, potl_index=0, a=None, repeat=1):
+    def D03_anti_bFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[1] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('bAl').a(a).repeat(repeat).string,
-            structure=structure
+            structure=structure,
+            pressure=pressure
         )
 
-    def D03_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=1):
+    def D03_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=1, pressure=0):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[3] = 'Fe'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('Fe').a(a).repeat(repeat).string,
-            structure=structure
+            structure=structure,
+            pressure=pressure
         )
