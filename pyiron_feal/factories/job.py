57282172678793b373c9cc074bd28650d4e8d5e1
Liam Huber <liam.huber@gmail.com>
2021-06-23 22:49:24 +0200

    Apply new job creators to do get energies

--- pyiron_feal/factories/job.py
+++ pyiron_feal/factories/job.py
@@ -32,111 +32,124 @@ class _Minimize(HasProject):
         super().__init__(project)
         self.name = JobName('min')
 
-    def _lammps_minimization(self, potl_index, name, structure, pressure):
-        job = self.project.create.job.Lammps(name)
+    def _lammps_minimization(self, potl_index, name, structure, pressure, delete_existing_job=False):
+        job = self.project.create.job.Lammps(name, delete_existing_job=delete_existing_job)
         job.structure = structure
         job.potential = self.project.input.potentials[potl_index]
         job.calc_minimize(pressure=pressure)
         return job
 
-    def BCC(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def BCC(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).BCC.a(a).repeat(repeat).string,
             structure=self.project.create.structure.FeAl.BCC(a=a).repeat(repeat),
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def FCC(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def FCC(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).FCC.a(a).repeat(repeat).string,
             structure=self.project.create.structure.FeAl.FCC(a=a).repeat(repeat),
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def random_BCC(self, potl_index=0, a=None, repeat=2, Al_at_frac=None, pressure=0):
+    def random_BCC(
+            self, potl_index=0, a=None, repeat=2, Al_at_frac=None, pressure=0, trial=None, delete_existing_job=False
+    ):
         return self._lammps_minimization(
             potl_index=potl_index,
-            name=self.name.potl(potl_index).random_BCC.a(a).repeat(repeat).c_Al(Al_at_frac).string,
+            name=self.name.potl(potl_index).random_BCC.a(a).repeat(repeat).c_Al(Al_at_frac).trial(trial).string,
             structure=self.project.create.structure.FeAl.random_BCC(a=a, repeat=repeat, Al_at_frac=Al_at_frac),
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def B2(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def B2(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).B2.a(a).repeat(repeat).string,
             structure=self.project.create.structure.FeAl.B2(a=a).repeat(repeat),
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def D03(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def D03(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.a(a).repeat(repeat).string,
             structure=self.project.create.structure.FeAl.D03(a=a).repeat(repeat),
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def dilute(self, potl_index=0, a=None, repeat=2, pressure=0):
+    def dilute(self, potl_index=0, a=None, repeat=2, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.BCC(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).BCC.append('Al').a(a).repeat(repeat).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def B2_anti_Fe_to_Al(self, potl_index=0, a=None, repeat=2, pressure=0):
+    def B2_anti_Fe_to_Al(self, potl_index=0, a=None, repeat=2, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.B2(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).B2.append('Al').a(a).repeat(repeat).P(pressure).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def B2_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=2, pressure=0):
+    def B2_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=2, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.B2(a=a).repeat(repeat)
         structure[1] = 'Fe'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).B2.append('Fe').a(a).repeat(repeat).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def D03_anti_aFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def D03_anti_aFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[0] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('aAl').a(a).repeat(repeat).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
     # The two unique Fe sites were found in a by-hand analysis of the chemistry of the first two neighbour shells and
     # then a brute-force calculation of the energy which simply confirmed the observed symmetries.
-    def D03_anti_bFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def D03_anti_bFe_to_Al(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[1] = 'Al'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('bAl').a(a).repeat(repeat).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
 
-    def D03_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=1, pressure=0):
+    def D03_anti_Al_to_Fe(self, potl_index=0, a=None, repeat=1, pressure=0, delete_existing_job=False):
         structure = self.project.create.structure.FeAl.D03(a=a).repeat(repeat)
         structure[3] = 'Fe'
         return self._lammps_minimization(
             potl_index=potl_index,
             name=self.name.potl(potl_index).D03.append('Fe').a(a).repeat(repeat).string,
             structure=structure,
-            pressure=pressure
+            pressure=pressure,
+            delete_existing_job=delete_existing_job
         )
--- pyiron_feal/subroutines/zerok.py
+++ pyiron_feal/subroutines/zerok.py
@@ -5,6 +5,7 @@
 from pyiron_feal.utils import HasProject, JobName
 import numpy as np
 from pandas import DataFrame
+from functools import lru_cache
 from scipy.constants import physical_constants
 KB = physical_constants['Boltzmann constant in eV/K'][0]
 import matplotlib.pyplot as plt
@@ -27,6 +28,160 @@ class ZeroK(HasProject):
         super().__init__(project)
         self._job_dict = None
         self._results = _Results(self)
+
+    @staticmethod
+    def _get_peratom_energy(job_creator, potl_index=0, run_again=False, **other_job_kwargs):
+        job = job_creator(potl_index=potl_index, delete_existing_job=run_again, **other_job_kwargs)
+        job.run()
+        return job.output.energy_pot[-1] / len(job.structure)
+
+    @lru_cache()
+    def get_BCC_peratom_energy(self, potl_index=0, run_again=False, **other_job_kwargs):
+        return self._get_peratom_energy(
+            self.project.create.job.minimize.BCC,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_FCC_peratom_energy(self, potl_index=0, run_again=False, **other_job_kwargs):
+        return self._get_peratom_energy(
+            self.project.create.job.minimize.FCC,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_B2_peratom_energy(self, potl_index=0, run_again=False, **other_job_kwargs):
+        return self._get_peratom_energy(
+            self.project.create.job.minimize.B2,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_D03_peratom_energy(self, potl_index=0, run_again=False, **other_job_kwargs):
+        return self._get_peratom_energy(
+            self.project.create.job.minimize.D03,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @staticmethod
+    def _get_size_converged_point_defect_energy(
+            ref_energy_per_atom, job_creator, stderr=1e-3, potl_index=0, run_again=False, **other_job_kwargs
+    ):
+        reps = 0
+        actual_err = np.inf
+        energies = [np.inf]
+        while actual_err > stderr:
+            reps += 1
+            job = job_creator(
+                potl_index=potl_index,
+                repeat=reps,
+                delete_existing_job=run_again,
+                **other_job_kwargs
+            )
+            job.run()
+            energies.append(job.output.energy_pot[-1] - len(job.structure) * ref_energy_per_atom)
+            actual_err = np.abs(energies[-1] - energies[-2])
+        return energies[-1], reps, actual_err
+
+    @lru_cache()
+    def get_formation_energy(self, stderr=1e-3, run_again=False, potl_index=0, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_BCC_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.dilute,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_B2_antisite_energy_Fe_to_Al(self, potl_index=0, stderr=1e-3, run_again=False, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_B2_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.B2_anti_Fe_to_Al,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_B2_antisite_energy_Al_to_Fe(self, potl_index=0, stderr=1e-3, run_again=False, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_B2_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.B2_anti_Al_to_Fe,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_D03_antisite_energy_Al_to_Fe(self, potl_index=0, stderr=1e-3, run_again=False, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_D03_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.D03_anti_Al_to_Fe,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_D03_antisite_energy_aFe_to_Al(self, potl_index=0, stderr=1e-3, run_again=False, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_D03_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.D03_anti_aFe_to_Al,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_D03_antisite_energy_bFe_to_Al(self, potl_index=0, stderr=1e-3, run_again=False, **other_job_kwargs):
+        return self._get_size_converged_point_defect_energy(
+            self.get_D03_peratom_energy(potl_index=potl_index, run_again=run_again, **other_job_kwargs),
+            self.project.create.job.minimize.D03_anti_bFe_to_Al,
+            stderr=stderr,
+            potl_index=potl_index,
+            run_again=run_again,
+            **other_job_kwargs
+        )
+
+    @lru_cache()
+    def get_solid_solution_repeats(self, potl_index=0, stderr=1e-3, run_again=False, n_trials=10, c_Al=None):
+        c_Al = self.project.input.experimental_data['c_Al'] if c_Al is None else c_Al
+        reps = 0
+        actual_err = np.inf
+        while actual_err > stderr:
+            reps += 1
+            if int(2 * reps ** 3 * c_Al) < 1:
+                continue
+            print(f"running {reps} repetitions...")
+            energies = []
+            for n in range(n_trials):
+                print(f"\ttrial {n}")
+                job = self.project.create.job.minimize.random_BCC(
+                    potl_index=potl_index,
+                    repeat=reps,
+                    Al_at_frac=c_Al,
+                    trial=n,
+                    delete_existing_job=run_again
+                )
+                job.run()
+                energies.append(job.output.energy_pot[-1] / len(job.structure))
+            actual_err = np.std(energies) / np.sqrt(n_trials)
+            print(f"stderr = {actual_err}")
+        return reps, actual_err
 
     @property
     def results(self):
