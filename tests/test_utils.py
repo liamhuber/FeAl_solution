73752d631a03a67981c44043cd2ca6a9776522be
Liam Huber <liam.huber@gmail.com>
2021-08-13 12:24:55 +0200

    Fix job name tests to use the new shorter names

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,24 +29,24 @@ class TestJobName(TestCase):
         self.assertEqual('foo_273_0K', JobName('foo').T(273.0))
 
     def test_other_tags(self):
-        self.assertEqual('foo_potl42', JobName('foo').potl(42))
+        self.assertEqual('foo_p42', JobName('foo').potl(42))
         self.assertEqual('foo_cAl33_33', JobName('foo').c_Al(0.33333333))
         self.assertEqual('foo_cAl33_333', JobName('foo').c_Al(0.33333333, ndigits=3))
-        self.assertEqual('foo_rep4', JobName('foo').repeat(4))
+        self.assertEqual('foo_r4', JobName('foo').repeat(4))
         self.assertEqual('foo', JobName('foo').repeat(None))
-        self.assertEqual('foo_trl3', JobName('foo').trial(3))
+        self.assertEqual('foo_t3', JobName('foo').trial(3))
         self.assertEqual('foo_bcc', JobName('foo').bcc)
         self.assertEqual('foo_fcc', JobName('foo').fcc)
         self.assertEqual('foo_b2', JobName('foo').b2)
         self.assertEqual('foo_d03', JobName('foo').d03)
         self.assertEqual('foo_P0_0', JobName('foo').P(0.))
         self.assertEqual('foo_a4_36', JobName('foo').a(4.355))
-        self.assertEqual('foo_cDAl2Fedil', JobName('foo').c_D03_anti_Al_to_Fe('Dilute'))
+        self.assertEqual('foo_cDAldil', JobName('foo').c_D03_anti_Al_to_Fe('Dilute'))
 
     def test_call(self):
         name = JobName('foo')
         self.assertEqual(
-            'foo_potl42_bcc_rep4_trl3_273K_P0_0_cDAl2Fe11_1',
+            'foo_p42_bcc_r4_t3_273K_P0_0_cDAl11_1',
             name(
                 potl_index=42,
                 bcc=True,
